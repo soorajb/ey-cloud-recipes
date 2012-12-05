66770cec680f5cfa0879384686680ba9bfa4f541
Sooraj <kasaragodsooraj@gmail.com>
2012-12-05 16:50:46 +0530

    add log imagemagick

--- cookbooks/imagemagick_6_4_9/recipes/default.rb
+++ cookbooks/imagemagick_6_4_9/recipes/default.rb
@@ -3,9 +3,13 @@
 # Recipe:: default
 #
 package "media-gfx/imagemagick" do
-	 version "6.4.9.2"
-	 action :upgrade
+ version "6.4.9.2"
+ action :upgrade
 end
 enable_package "media-gfx/imagemagick" do
-	  version "6.4.9.2"
+ version "6.4.9.2"
 end
+ey_cloud_report "imagemagick_6_4_9" do 
+ message "imagemagick 6.4.9" 
+end 
+Chef::Log.info "Done with ImageMagick 6.4.9" 
